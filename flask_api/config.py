9df7d402da8466317711a311544d894b196f51f4
Raúl González Martínez <raulgonz@mercadona.es>
2017-03-27 11:11:08 +0200

    Database config added

--- flask_api/config.py
+++ flask_api/config.py
@@ -1,9 +1,11 @@
+# Python imports
+import os
+
 # Statement for enabling the development environment
 DEBUG = True
 
 # Define the application directory
-import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Application threads. A common general assumption is
 # using 2 per available processor cores - to handle
@@ -12,11 +14,15 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 THREADS_PER_PAGE = 2
 
 # Enable protection agains *Cross-site Request Forgery (CSRF)*
-CSRF_ENABLED     = True
+CSRF_ENABLED = True
 
 # Use a secure, unique and absolutely secret key for
-# signing the data. 
+# signing the data.
 CSRF_SESSION_KEY = "secret"
 
 # Secret key for signing cookies
 SECRET_KEY = "secret"
+
+# DATABASE
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/mercadona_db'
+SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
